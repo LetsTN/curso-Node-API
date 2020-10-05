11e7872615562477ba631184ae4fff66785dafa8
Letícia Teixeira do Nascimento <leticia.teix.nas@gmail.com>
2020-10-05 18:24:08 -0300

    C03P02: agrupando forecast por hora :sparkles:

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -15,12 +15,17 @@ export interface Beach {
   user: string;
 }
 
+export interface TimeForecast {
+  time: string;
+  forecast: BeachForecast[];
+}
+
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
 
 export class Forecast {
   constructor(protected stormGlass = new StormGlass()) {}
 
-  public async processForecastForBeaches(beaches: Beach[]): Promise<BeachForecast[]> {
+  public async processForecastForBeaches(beaches: Beach[]): Promise<TimeForecast[]> {
     const pointsWithCorrectSources: BeachForecast[] = [];
 
     for(const beach of beaches) {
@@ -39,6 +44,25 @@ export class Forecast {
       pointsWithCorrectSources.push(...enrichedBeachData);
     }
     
-    return pointsWithCorrectSources;
+    return this.maoForecastByTime(pointsWithCorrectSources);
+  }
+
+  private maoForecastByTime(forecast: BeachForecast[]): TimeForecast[]{
+    const forecastByTime: TimeForecast[] = [];
+
+    for (const point of forecast) {
+      const timePoint = forecastByTime.find((f) => f.time === point.time);
+
+      if (timePoint) {
+        timePoint.forecast.push(point);
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point]
+        });
+      }
+    }
+
+    return forecastByTime;
   }
 }
