8562f7b75369b3f736eef7f325de614bc54657ad
Letícia Teixeira do Nascimento <leticia.nascimento@etaure.srv>
2020-10-22 14:01:12 -0300

    C05P01: criando o User Model :card_file_box:

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import * as database from '@src/database';
 
 import { ForecastController } from './controllers/forecast';
 import { BeachesController } from './controllers/beaches';
+import { UsersController } from './controllers/users';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -26,10 +27,12 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     const beachesController = new BeachesController();
+    const usersController = new UsersController()
 
     this.addControllers([
       forecastController,
-      beachesController
+      beachesController,
+      usersController
     ]);
   }
 
---
+++ test/functional/users.test.ts
@@ -0,0 +1,24 @@
+import { User } from  '@src/models/user';
+
+describe('Users functional tests', () => {
+  beforeEach(async () => {
+    await User.deleteMany({});
+  });
+
+  describe('when creating a new user', () => {
+    it('should succesfully create a new user', async () => {
+      const newUser = {
+        name: 'John Doe', 
+        email: 'john@mail.com',
+        password: '1234'
+      };
+
+      const response = await global.testRequest.post('/users').send(newUser);
+      expect(response.status).toBe(201);
+
+      expect(response.body).toEqual(expect.objectContaining(newUser));
+    });
+
+    
+  });
+});
