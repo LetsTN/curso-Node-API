1f0d5051abe67df6e177d96a0f46f83ca5c691e3
Letícia Teixeira do Nascimento <leticia.nascimento@etaure.srv>
2020-10-22 14:03:13 -0300

    C05P02: fazendo validators para o User :sparkles:

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,14 +1,20 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { User } from '@src/models/user';
+import { BaseController } from './index';
 
 @Controller('users')
-export class UsersController {
+export class UsersController extends BaseController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    const user = new User(req.body);
-    const newUser = await user.save();
+    try {
+      const user = new User(req.body);
+      const newUser = await user.save();
 
-    res.status(201).send(newUser);
+      res.status(201).send(newUser);
+    } catch (error) {
+      this.sendCreateUpdateErrorResponse(res, error);
+    }
+    
   }
 }
