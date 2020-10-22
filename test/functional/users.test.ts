d83a88f0bb5aa7e4d727f59f628bb904ce8c4335
Letícia Teixeira do Nascimento <leticia.nascimento@etaure.srv>
2020-10-22 14:54:20 -0300

    C05P03: encriptando a senha do User e criando o serviço de autenticação :sparkles:

--- test/functional/users.test.ts
+++ test/functional/users.test.ts
@@ -1,4 +1,6 @@
 import { User } from  '@src/models/user';
+import AuthService from '@src/services/auth';
+
 
 describe('Users functional tests', () => {
   beforeEach(async () => {
@@ -6,7 +8,7 @@ describe('Users functional tests', () => {
   });
 
   describe('when creating a new user', () => {
-    it('should succesfully create a new user', async () => {
+    it('should successfully create a new user with encrypted password', async () => {
       const newUser = {
         name: 'John Doe', 
         email: 'john@mail.com',
@@ -16,7 +18,15 @@ describe('Users functional tests', () => {
       const response = await global.testRequest.post('/users').send(newUser);
       expect(response.status).toBe(201);
 
-      expect(response.body).toEqual(expect.objectContaining(newUser));
+      await expect(AuthService.comparePasswords(
+        newUser.password, 
+        response.body.password
+      )).resolves.toBeTruthy();
+      
+      expect(response.body).toEqual(expect.objectContaining({
+        ...newUser,
+        ...{ password: expect.any(String) },
+      }));
     });
 
     it('Should return 422 when there is a validation error', async () => {
