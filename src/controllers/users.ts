57533437f229e9ff26ee74977710ab2c3c729816
Letícia Teixeira do Nascimento <leticia.nascimento@etaure.srv>
2020-10-22 15:35:47 -0300

    C05P04: autenticação com JWT :sparkles:

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,7 @@
 import { Controller, Post } from '@overnightjs/core';
 import { Request, Response } from 'express';
 import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 import { BaseController } from './index';
 
 @Controller('users')
@@ -15,6 +16,26 @@ export class UsersController extends BaseController {
     } catch (error) {
       this.sendCreateUpdateErrorResponse(res, error);
     }
-    
+  }
+
+  @Post('authenticate')
+  public async authenticate(req: Request, res: Response): Promise<Response> {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return res.status(401).send({
+        code: 401,
+        error: 'User not found!',
+      });
+    }
+    if (
+      !(await AuthService.comparePasswords(req.body.password, user.password))
+    ) {
+      return res
+        .status(401)
+        .send({ code: 401, error: 'Password does not match!' });
+    }
+    const token = AuthService.generateToken(user.toJSON());
+
+    return res.send({ ...user.toJSON(), ...{ token } });
   }
 }
